130db3518bb09d8e1d61ca0169de0a67e6888e74
Emmanuel <ememeemmanuel3@gmail.com>
2023-01-12 21:33:44 +0000

    Level 4 - Salting and Hashing Passwords with bcrypt

--- app.js
+++ app.js
@@ -4,12 +4,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5")
 
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
-
-console.log(md5("123456"));
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
@@ -26,7 +25,6 @@ const userSchema = new mongoose.Schema({
   password: String,
 });
 
-
 const User = new mongoose.model("User", userSchema);
 
 app.get("/", (req, res) => {
@@ -42,30 +40,36 @@ app.get("/login", (req, res) => {
 });
 
 app.post("/register", (req, res) => {
-  const newUser = new User({
-    email: req.body.username,
-    password: md5(req.body.password)
-  });
-  newUser.save((err) => {
-    if (err) {
-      console.log(err);
-    } else {
-      res.render("secrets");
-    }
+  bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+    const newUser = new User({
+      email: req.body.username,
+      password: hash,
+    });
+    newUser.save((err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.render("secrets");
+      }
+    });
   });
 });
 
 app.post("/login", (req, res) => {
   const username = req.body.username;
-  const password = md5(req.body.password)
+  const password = req.body.password;
 
   User.findOne({ email: username }, (err, foundUser) => {
     if (err) {
       console.log(err);
     } else {
       if (foundUser) {
-        if (foundUser.password === password) {
-          res.render("secrets");
+        if (foundUser.password) {
+          bcrypt.compare(password, foundUser.password, function (err, result) {
+            if (result === true) {
+              res.render("secrets");
+            }
+          });
         } else {
           res.send("<h1>Incorrect login details</h1>");
         }
