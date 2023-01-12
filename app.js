206ab2c84ccf3b9332c2d8344ddbf5fd8c8bddcd
Emmanuel <ememeemmanuel3@gmail.com>
2023-01-12 10:56:20 +0000

    Level 1 - Register Users with Username and Password

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -12,6 +13,64 @@ app.use(
     extended: true,
   })
 );
+
+mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
+
+const userSchema = {
+  email: String,
+  password: String,
+};
+
+const User = new mongoose.model("User", userSchema);
+
+
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+
+app.post("/register", (req, res)=>{
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    })
+    newUser.save((err)=>{
+        if(err){
+            console.log(err)
+        }else{
+            res.render("secrets")
+        }
+    })
+})
+
+app.post("/login", (req, res)=>{
+    const username = req.body.username
+    const password = req.body.password
+
+    User.findOne({email: username}, (err, foundUser)=>{
+        if(err){
+            console.log(err)
+        }else{
+            if(foundUser){
+                if(foundUser.password === password){
+                    res.render("secrets")
+                }else{
+                    res.send("<h1>Incorrect login details</h1>")
+                }
+            }
+
+        }
+    })
+})
 
 app.listen(3000, () => {
   console.log("Server started on port 3000.");
