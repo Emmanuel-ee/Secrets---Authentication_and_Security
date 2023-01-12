ebca473619846e42b2cf94ae71b017686d5871e4
Emmanuel <ememeemmanuel3@gmail.com>
2023-01-12 11:56:43 +0000

    Level 2 - Database Encryption &  Using Environment Variables to Keep Secrets Safe

---
+++ README.md
@@ -0,0 +1,3 @@
+# .env file should contain the secret key in format - SECRET=xxxxxxxxxxx
+
+# Add a .gitignore file to the project and ensure the .emv file is included.
--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 //jshint esversion:6
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 
 const app = express();
+
+console.log(process.env.API_KEY);
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
@@ -16,18 +20,14 @@ app.use(
 
 mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
 
-const userSchema = {
+const userSchema = new mongoose.Schema({
   email: String,
   password: String,
-};
+});
+
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
 
 const User = new mongoose.model("User", userSchema);
-
-
-
-app.get("/login", (req, res) => {
-  res.render("login");
-});
 
 app.get("/", (req, res) => {
   res.render("home");
@@ -37,40 +37,42 @@ app.get("/register", (req, res) => {
   res.render("register");
 });
 
+app.get("/login", (req, res) => {
+  res.render("login");
+});
 
-app.post("/register", (req, res)=>{
-    const newUser = new User({
-        email: req.body.username,
-        password: req.body.password
-    })
-    newUser.save((err)=>{
-        if(err){
-            console.log(err)
-        }else{
-            res.render("secrets")
+app.post("/register", (req, res) => {
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password,
+  });
+  newUser.save((err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("secrets");
+    }
+  });
+});
+
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({ email: username }, (err, foundUser) => {
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUser) {
+        if (foundUser.password === password) {
+          res.render("secrets");
+        } else {
+          res.send("<h1>Incorrect login details</h1>");
         }
-    })
-})
-
-app.post("/login", (req, res)=>{
-    const username = req.body.username
-    const password = req.body.password
-
-    User.findOne({email: username}, (err, foundUser)=>{
-        if(err){
-            console.log(err)
-        }else{
-            if(foundUser){
-                if(foundUser.password === password){
-                    res.render("secrets")
-                }else{
-                    res.send("<h1>Incorrect login details</h1>")
-                }
-            }
-
-        }
-    })
-})
+      }
+    }
+  });
+});
 
 app.listen(3000, () => {
   console.log("Server started on port 3000.");
