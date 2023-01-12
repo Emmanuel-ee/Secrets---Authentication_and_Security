b2906be0d2e616cc2160f6a9abf93831d048004b
Emmanuel <ememeemmanuel3@gmail.com>
2023-01-12 12:23:23 +0000

    Level 3 - Hashing Passwords

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # .env file should contain the secret key in format - SECRET=xxxxxxxxxxx
 
-# Add a .gitignore file to the project and ensure the .emv file is included.
+# Add a .gitignore file to the project and ensure the .env file is included.
--- app.js
+++ app.js
@@ -4,11 +4,12 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5")
+
 
 const app = express();
 
-console.log(process.env.API_KEY);
+console.log(md5("123456"));
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
@@ -25,7 +26,6 @@ const userSchema = new mongoose.Schema({
   password: String,
 });
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
 
 const User = new mongoose.model("User", userSchema);
 
@@ -44,7 +44,7 @@ app.get("/login", (req, res) => {
 app.post("/register", (req, res) => {
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password,
+    password: md5(req.body.password)
   });
   newUser.save((err) => {
     if (err) {
@@ -57,7 +57,7 @@ app.post("/register", (req, res) => {
 
 app.post("/login", (req, res) => {
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password)
 
   User.findOne({ email: username }, (err, foundUser) => {
     if (err) {
